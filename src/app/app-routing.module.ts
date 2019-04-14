c8e70aa55bee5fbb0eac3bf1a6d9d6213f108a20
unknown <yellukarttik@gmail.com>
2019-04-14 02:18:25 -0500

    started landing work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegistrationUserComponent } from './registration-user/registration-user.component';
 import { LoginUserComponent } from './login-user/login-user.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { PostComponent } from './post/post.component';
+import { SearchComponent } from './search/search.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { MessagingComponent } from './messaging/messaging.component';
+import { ViewAccountComponent } from './view-account/view-account.component';
+
 const routes: Routes = [
   {path:'register' , component: RegistrationUserComponent},
   {path:'login' , component: LoginUserComponent},
-  {path:'landing' , component: LandingPageComponent}
+  {path:'landing' , component: LandingPageComponent,
+children:[
+  {path: 'notifications',component:NotificationsComponent },
+  {path: 'post',component:PostComponent },
+  {path: 'search',component:SearchComponent },
+  {path: 'privacy',component:PrivacyComponent },
+  {path: 'messaging',component:MessagingComponent },
+  {path: 'viewaccount',component:ViewAccountComponent }
+]}
+
 ];
 
 @NgModule({
@@ -16,4 +32,6 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [RegistrationUserComponent, LoginUserComponent, LandingPageComponent];
+export const routingComponents = [RegistrationUserComponent,
+  LoginUserComponent, LandingPageComponent,NotificationsComponent,PostComponent,SearchComponent,PrivacyComponent,
+ MessagingComponent, ViewAccountComponent];
---
+++ src/app/view-account/view-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-account',
+  templateUrl: './view-account.component.html',
+  styleUrls: ['./view-account.component.css']
+})
+export class ViewAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
